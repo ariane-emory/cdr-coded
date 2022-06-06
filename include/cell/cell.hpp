9b356bd1cfb9955306043703c91b576530871d23
Ariane Emory <ariane.emory@protonmail.com>
2022-06-06 19:40:22 -0400

    ...

--- include/cell/cell.hpp
+++ include/cell/cell.hpp
@@ -22,13 +22,12 @@ namespace reseune {
         }
 
         static constexpr uint8_t   FLAG_BITS            = 2;
-        static constexpr uint8_t   VALUE_BITS           = 64-FLAG_BITS;
+        static constexpr uint8_t   VALUE_BITS           = 64 - FLAG_BITS;
         static constexpr uintptr_t MASK_VALUE           = 0b00111111'11111111'11111111'11111111'11111111'11111111'11111111'11111111ul;
         static constexpr uintptr_t MASK_FLAG            = ~MASK_VALUE;
-        static constexpr uintptr_t FLAG_MASK_VALUE      = cell_type::element      << VALUE_BITS;
-        static constexpr uintptr_t FLAG_MASK_LAST_VALUE = cell_type::last_element << VALUE_BITS;
-        static constexpr uintptr_t FLAG_MASK_REST       = cell_type::rest         << VALUE_BITS;
-        
+        static constexpr uintptr_t FLAG_MASK_VALUE      = cell_type_to_mask(cell_type::element);
+        static constexpr uintptr_t FLAG_MASK_LAST_VALUE = cell_type_to_mask(cell_type::last_element);
+        static constexpr uintptr_t FLAG_MASK_REST       = cell_type_to_mask(cell_type::rest);
 
         uintptr_t data;
 
