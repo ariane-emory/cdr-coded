42682fb3c88648e37004a21d4661983a7c73782e
Ariane Emory <ariane.emory@protonmail.com>
2022-06-07 09:55:44 -0400

    ...

--- include/reseune/cell/cell.hpp
+++ include/reseune/cell/cell.hpp
@@ -136,10 +136,10 @@ namespace reseune {
     struct const_iterator
     {
       typedef std::forward_iterator_tag iterator_category;
-      typedef std::ptrdiff_t difference_type  ;
-      typedef cell value_type       ;
-      typedef const cell * pointer_type     ;  // or also value_type*
-      typedef const cell & reference_type   ;
+      typedef std::ptrdiff_t            difference_type ;
+      typedef cell                      value_type;
+      typedef cell const *              pointer_type; 
+      typedef cell const &              reference_type;
       
       const_iterator(pointer_type ptr): m_ptr(ptr) {}
       
