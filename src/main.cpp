c026191a897830659b87b459bd1ed8708879d3bf
Ariane Emory <ariane.emory@protonmail.com>
2022-06-06 23:17:45 -0400

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,22 @@
 using namespace reseune;
 
 void describe_some_sizes() {
-    printf("sizeof void *             = %zu\n", sizeof(void *));
-    printf("sizeof uintptr_t          = %zu\n", sizeof(uintptr_t));
-    printf("sizeof unsigned long      = %zu\n", sizeof(unsigned long));
-    printf("sizeof unsigned long long = %zu\n", sizeof(unsigned long long));
-    printf("\n");
+  printf("sizeof void *             = %zu\n", sizeof(void *));
+  printf("sizeof uintptr_t          = %zu\n", sizeof(uintptr_t));
+  printf("sizeof unsigned long      = %zu\n", sizeof(unsigned long));
+  printf("sizeof unsigned long long = %zu\n", sizeof(unsigned long long));
+  printf("\n");
 }
 
 int main() {
-    describe_some_sizes();
+  describe_some_sizes();
     
-    cell acell(33, cell::cell_type::element);
-    cell bcell(&acell, cell::cell_type::rest);
+  cell acell(33, cell::cell_type::element);
+  cell bcell(&acell, cell::cell_type::rest);
 
-    cell::describe_class();
-    acell.describe_instance();
-    bcell.describe_instance();
+  cell::describe_class();
+  acell.describe_instance();
+  bcell.describe_instance();
     
 }
 
