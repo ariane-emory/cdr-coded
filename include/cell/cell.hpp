91d8ff837c6263551cd78f6884bb6a916c95849c
Ariane Emory <ariane.emory@protonmail.com>
2022-06-06 23:12:06 -0400

    ...

--- include/cell/cell.hpp
+++ include/cell/cell.hpp
@@ -7,155 +7,154 @@
 #include <cassert>
 
 namespace reseune {
-    class cell {
-    public:
-        typedef uintptr_t value_type;
-        typedef uint8_t uchar_type;
+  class cell {
+  public:
+    typedef uintptr_t value_type;
+    typedef uint8_t uchar_type;
 
-        enum class cell_type : uintptr_t {
-            element = 1,
-            last_element,
-            rest,
-            invalid
-        };
-
-        static constexpr uchar_type FLAG_BITS_COUNT      = 2;
-        static constexpr uchar_type VALUE_BITS_COUNT     = (sizeof(value_type) << 3) - FLAG_BITS_COUNT;
-        static constexpr value_type MASK_VALUE           = (1ul << VALUE_BITS_COUNT) - 1;
-        static constexpr value_type MASK_FLAG            = ~MASK_VALUE;
-        static constexpr value_type FLAG_MASK_VALUE      = static_cast<value_type>(cell_type::element)      << VALUE_BITS_COUNT;
-        static constexpr value_type FLAG_MASK_LAST_VALUE = static_cast<value_type>(cell_type::last_element) << VALUE_BITS_COUNT;
-        static constexpr value_type FLAG_MASK_REST       = static_cast<value_type>(cell_type::rest)         << VALUE_BITS_COUNT;
-
-        value_type data;
-
-        inline constexpr
-        cell(value_type const & v, cell_type const & ct) {
-            assert(ct != cell_type::rest);
-            
-            data = v | (static_cast<value_type>(ct) << VALUE_BITS_COUNT);
-        }
-        
-        inline constexpr
-        cell(cell * const v, cell_type const & ct) {
-            assert(ct == cell_type::rest);
-            
-            data = std::bit_cast<value_type>(v) | (static_cast<value_type>(ct) << VALUE_BITS_COUNT);
-        }
-
-        inline constexpr
-        cell() {
-            data = 0 | (static_cast<value_type>(cell_type::invalid) << VALUE_BITS_COUNT);
-        }
-        
-
-        inline constexpr
-        value_type value() const {
-            assert(! is_type(cell_type::rest));
-            
-            return get_value();
-        }
-
-        inline constexpr
-        cell const * rest() const {
-            assert(is_type(cell_type::rest));
-            
-            return get_rest();
-        }
-        
-        inline constexpr
-        value_type flag() const {
-            return data & MASK_FLAG;
-        }
-
-        inline constexpr
-        cell_type type() const {
-            return static_cast<cell_type>(flag() >> 62); 
-        }
-
-        inline constexpr
-        bool is_type(cell_type const & ct) const {
-            return type() == ct; 
-        }
-        
-        static inline
-        void describe_class() {
-            print("VB:                   ", VALUE_BITS_COUNT);
-            print("FB:                   ", FLAG_BITS_COUNT);
-            print_bits("MASK_FLAGS:           ", MASK_VALUE, false);
-            print_bits("MASK_VALUE:           ", MASK_FLAG, false);
-            print_bits("FLAG_MASK_VALUE:      ", FLAG_MASK_VALUE, false);
-            print_bits("FLAG_MASK_LAST_VALUE: ", FLAG_MASK_LAST_VALUE, false);
-            print_bits("FLAG_MASK_REST:       ", FLAG_MASK_REST, false);
-            putchar('\n');
-        }
-        
-        inline
-        void describe_instance() const {
-            // printf("Cell at:                                                                                           %018p\n", this);
-            print_bits("cell @                ", reinterpret_cast<uintptr_t>(this));
-            print_bits("cell.data:            ", data);
-            print_bits("cell.flag():          ", flag());
-            print_bits("cell.type():          ", type());
-            if (is_type(cell_type::rest))
-                print_bits("cell.rest():          ", reinterpret_cast<uintptr_t>(rest()));
-            else
-                print_bits("cell.value()          ", value());
-            putchar('\n');
-        }
-        
-    private:
-        inline constexpr
-        value_type get_value() const {
-            return data & MASK_VALUE;
-        }
-
-        inline constexpr
-        cell const * get_rest() const {
-            return std::bit_cast<cell const *>(get_value());
-        }
-
-        template <typename T>
-        static inline
-        void print_bits(
-            char const * descr, T const & v, bool const & print_int = true) {
-            printf("%s 0b", descr);
-
-            value_type tmp = static_cast<value_type>(v);
-            
-            {
-                uchar_type ix = 0;
-            
-                for (value_type mask = 0b10000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000ul;
-                     mask;
-                     mask >>= 1) {
-                    putchar((mask & tmp) ? '1' : '0');
-
-                    ++ix %= 8;
-                    if (0 == ix && mask > 1)
-                        putchar('\'');
-                }
-            }
-            
-            if (print_int)
-                printf(" = 0x%016lx = % 24lu\n", tmp, tmp);
-            else
-                putchar('\n');
-        }
-
-        static inline
-        void print(char const * descr, uchar_type const & v) {
-            printf("%s %u\n", descr, v);
-        }
+    enum class cell_type : uintptr_t {
+      element = 1,
+      last_element,
+      rest,
+      invalid
     };
 
-    class conspair {
-    private:
-        cell car, cdr;
-    public:
-        inline constexpr
-        conspair() {}
-    };
+    static constexpr uchar_type FLAG_BITS_COUNT      = 2;
+    static constexpr uchar_type VALUE_BITS_COUNT     = (sizeof(value_type) << 3) - FLAG_BITS_COUNT;
+    static constexpr value_type MASK_VALUE           = (1ul << VALUE_BITS_COUNT) - 1;
+    static constexpr value_type MASK_FLAG            = ~MASK_VALUE;
+    static constexpr value_type FLAG_MASK_VALUE      = static_cast<value_type>(cell_type::element)      << VALUE_BITS_COUNT;
+    static constexpr value_type FLAG_MASK_LAST_VALUE = static_cast<value_type>(cell_type::last_element) << VALUE_BITS_COUNT;
+    static constexpr value_type FLAG_MASK_REST       = static_cast<value_type>(cell_type::rest)         << VALUE_BITS_COUNT;
+
+    value_type data;
+
+    inline constexpr
+    cell(value_type const & v, cell_type const & ct) {
+      // assert(ct != cell_type::rest);
+            
+      data = v | (static_cast<value_type>(ct) << VALUE_BITS_COUNT);
+    }
+        
+    inline constexpr
+    cell(cell * const v, cell_type const & ct) {
+      // assert(ct == cell_type::rest);
+            
+      data = std::bit_cast<value_type>(v) | (static_cast<value_type>(ct) << VALUE_BITS_COUNT);
+    }
+
+    inline constexpr
+    cell() {
+      data = 0 | (static_cast<value_type>(cell_type::invalid) << VALUE_BITS_COUNT);
+    }
+        
+
+    inline constexpr
+    value_type value() const {
+      assert(! is_type(cell_type::rest));
+            
+      return get_value();
+    }
+
+    inline constexpr
+    cell const * rest() const {
+      assert(is_type(cell_type::rest));
+            
+      return get_rest();
+    }
+        
+    inline constexpr
+    value_type flag() const {
+      return data & MASK_FLAG;
+    }
+
+    inline constexpr
+    cell_type type() const {
+      return static_cast<cell_type>(flag() >> 62); 
+    }
+
+    inline constexpr
+    bool is_type(cell_type const & ct) const {
+      return type() == ct; 
+    }
+        
+    static inline
+    void describe_class() {
+      print("VB:                   ", VALUE_BITS_COUNT);
+      print("FB:                   ", FLAG_BITS_COUNT);
+      print_bits("MASK_FLAGS:           ", MASK_VALUE, false);
+      print_bits("MASK_VALUE:           ", MASK_FLAG, false);
+      print_bits("FLAG_MASK_VALUE:      ", FLAG_MASK_VALUE, false);
+      print_bits("FLAG_MASK_LAST_VALUE: ", FLAG_MASK_LAST_VALUE, false);
+      print_bits("FLAG_MASK_REST:       ", FLAG_MASK_REST, false);
+      putchar('\n');
+    }
+        
+    inline
+    void describe_instance() const {
+      // printf("Cell at:                                                                                           %018p\n", this);
+      print_bits("cell @                ", reinterpret_cast<uintptr_t>(this));
+      print_bits("cell.data:            ", data);
+      print_bits("cell.flag():          ", flag());
+      print_bits("cell.type():          ", type());
+      if (is_type(cell_type::rest))
+        print_bits("cell.rest():          ", reinterpret_cast<uintptr_t>(rest()));
+      else
+        print_bits("cell.value()          ", value());
+      putchar('\n');
+    }
+        
+  private:
+    inline constexpr
+    value_type get_value() const {
+      return data & MASK_VALUE;
+    }
+
+    inline constexpr
+    cell const * get_rest() const {
+      return std::bit_cast<cell const *>(get_value());
+    }
+
+    template <typename T>
+    static inline
+    void print_bits(
+      char const * descr, T const & v, bool const & print_int = true) {
+      printf("%s 0b", descr);
+
+      value_type tmp = static_cast<value_type>(v);
+            
+      {
+        uchar_type ix = 0;
+            
+        for (value_type mask = 0b10000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000ul;
+             mask;
+             mask >>= 1) {
+          putchar((mask & tmp) ? '1' : '0');
+
+          ++ix %= 8;
+          if (0 == ix && mask > 1)
+            putchar('\'');
+        }
+      }
+            
+      if (print_int)
+        printf(" = 0x%016lx = % 24lu\n", tmp, tmp);
+      else
+        putchar('\n');
+    }
+
+    static inline
+    void print(char const * descr, uchar_type const & v) {
+      printf("%s %u\n", descr, v);
+    }
+  };
+
+  struct conspair {
+    cell car, cdr;
+
+    inline constexpr
+    conspair() {}
+  };
 }
 
 static constexpr reseune::cell nil { static_cast<reseune::cell::value_type>(0), reseune::cell::cell_type::rest };
