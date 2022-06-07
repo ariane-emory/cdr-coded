1100acb618b37ffa693b4aa3aec5090d6450a147
Ariane Emory <ariane.emory@protonmail.com>
2022-06-07 01:47:08 -0400

    ...

--- include/reseune/cell/cell.hpp
+++ include/reseune/cell/cell.hpp
@@ -190,7 +190,10 @@ namespace reseune {
     }
   };
 
-  // static constexpr cell nil { static_cast<reseune::cell::value_type>(0), reseune::cell::cell_type::rest };
+  static constinit cell nil {
+    static_cast<reseune::cell::value_type>(0),
+    reseune::cell::cell_type::rest
+  };
 
   struct conspair {
     cell car, cdr;
