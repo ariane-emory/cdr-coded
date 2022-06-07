897cef22b16fc64dfce56d7e0c1e5baa6863d7c1
Ariane Emory <ariane.emory@protonmail.com>
2022-06-07 02:30:39 -0400

    ...

---
+++ include/reseune/reseune.hpp
@@ -0,0 +1,22 @@
+#ifndef RESEUNE_HPP
+#define RESEUNE_HPP
+
+namespace reseune {
+  static constexpr bool THROW =
+#ifdef RESEUNE_THROW
+    true
+#else
+    false
+#endif
+    ;
+
+  static constexpr bool WARN =
+#ifdef RESEUNE_WARN
+    true
+#else
+    false
+#endif
+    ;
+}
+
+#endif
