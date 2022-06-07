63ea19015bd9df5e838b66361edbb108a4fb79d9
Ariane Emory <ariane.emory@protonmail.com>
2022-06-07 02:36:23 -0400

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,25 @@ void describe_some_sizes() {
 int main() {
   describe_some_sizes();
     
-  cell acell { 30, cell::cell_type::element };
-  cell bcell { &acell, cell::cell_type::link };
+  // cell acell { 30, cell::cell_type::element };
+  // cell bcell { &acell, cell::cell_type::link };
+
+  cell::describe_class();
+
+  // acell.describe_instance();
+  // bcell.describe_instance();
+  // (*bcell).describe_instance();
 
   cell cells[] = {
     { 10, cell::cell_type::element },
     { 20, cell::cell_type::element },
-    { 20, cell::cell_type::element }
+    { 30, cell::cell_type::element },
+    nil
   };
 
-
-  cell::describe_class();
-  acell.describe_instance();
-  bcell.describe_instance();
-  (*bcell).describe_instance();
-
+  for (cell i : cells) {
+    i.describe_instance();
+  }
 }
 
 
