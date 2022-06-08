bf37424e9c9ad3b3703e877d597b541aa738ca46
Ariane Emory <ariane.emory@protonmail.com>
2022-06-07 21:36:03 -0400

    ...

--- include/reseune/cell/cell.hpp
+++ include/reseune/cell/cell.hpp
@@ -198,6 +198,7 @@ namespace reseune {
         else if ((++m_ptr)->is_link()) {
           m_ptr = m_ptr->link();
         }
+        // else (i.e, if m_ptr->is_element()), it's value has already been incremented in the prior case.
       }
 
       // prefix
