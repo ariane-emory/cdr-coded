c0706c8ba9fb6387663fbd181578443fcec201dd
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 02:15:38 -0400

    ...

--- include/reseune/cell/cell.hpp
+++ include/reseune/cell/cell.hpp
@@ -267,51 +267,6 @@ namespace reseune {
       return tag() == ct; 
     }
 
-    template <typename T>
-    static
-    void print_bits(
-      char const * descr,
-      T const & v,
-      bool const & show_int = true,
-      bool const & show_bits = true) {
-
-      printf(
-        show_bits
-        ? "%s 0b"
-        : "%s   ",
-        descr);
-
-      T tmp { static_cast<T>(v) };
-            
-      {
-        uchar_type ix { 0 };
-            
-        for (T mask { static_cast<T>(1) << ((sizeof(T) << 3) - 1) };
-             mask;
-             mask >>= 1) {
-          if (show_bits)
-            putchar((mask & tmp) ? '1' : '0');
-          else
-            putchar(' ');
-
-          ++ix %= 8;
-
-          if (0 == ix && mask > 1) [[unlikely]]
-            putchar(show_bits
-                    ? '\''
-                    : ' ');
-        }
-      }
-            
-      if (show_int) [[likely]]
-        printf(show_bits
-               ? " = 0x%016lx = % 20lu\n"
-               : "   0x%016lx = % 20lu\n",
-               (uintptr_t)tmp,
-               (uintptr_t)tmp);
-      else [[unlikely]]
-        putchar('\n');
-    }
 
     static
     void print(char const * descr, uchar_type const & v) {
--- include/reseune/reseune.hpp
+++ include/reseune/reseune.hpp
@@ -2,21 +2,51 @@
 #define RESEUNE_HPP
 
 namespace reseune {
-//   static constexpr bool THROW =
-// #ifdef RESEUNE_THROW
-//     true
-// #else
-//     false
-// #endif
-//     ;
+  template <typename T>
+  static
+  void print_bits(
+    char const * descr,
+    T const & v,
+    bool const & show_int = true,
+    bool const & show_bits = true) {
 
-//   static constexpr bool WARN =
-// #ifdef RESEUNE_WARN
-//     true
-// #else
-//     false
-// #endif
-//     ;
+    printf(
+      show_bits
+      ? "%s 0b"
+      : "%s   ",
+      descr);
+
+    T tmp { static_cast<T>(v) };
+            
+    {
+      size_t ix { 0 };
+            
+      for (T mask { static_cast<T>(1) << ((sizeof(T) << 3) - 1) };
+           mask;
+           mask >>= 1) {
+        if (show_bits)
+          putchar((mask & tmp) ? '1' : '0');
+        else
+          putchar(' ');
+
+        ++ix %= 8;
+
+        if (0 == ix && mask > 1) [[unlikely]]
+          putchar(show_bits
+                  ? '\''
+                  : ' ');
+      }
+    }
+            
+    if (show_int) [[likely]]
+      printf(show_bits
+             ? " = 0x%016lx = % 20lu\n"
+             : "   0x%016lx = % 20lu\n",
+             (uintptr_t)tmp,
+             (uintptr_t)tmp);
+    else [[unlikely]]
+      putchar('\n');
+  }
 }
 
 #endif
