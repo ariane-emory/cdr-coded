72347a90da9d2e1f1961444c33553f16e4ec2293
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 00:33:51 -0400

    ...

--- include/reseune/mempool/mempool.hpp
+++ include/reseune/mempool/mempool.hpp
@@ -4,15 +4,17 @@
 namespace reseune {
   template <typename T, size_t S>
   class mempool : public pool<T, S> {
+    using value_type = T;
+    
     struct ll_t {
       struct ll_head *next;
       struct ll_head *prev;
     };
 
     struct alloc_node_t {
-      ll_t node;
+      ll_t   node;
       size_t size;
-      char * block;
+      T *    block;
     };
   };
 }
