d6a58f90c1b942bf829e16e07ba33977590068c6
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 15:44:48 -0400

    ...

---
+++ include/reseune/double_link/double_link.hpp
@@ -0,0 +1,104 @@
+#ifndef RESEUNE_DOUBLE_LINK_H
+#define RESEUNE_DOUBLE_LINK_H
+
+#include <cassert>
+
+namespace reseune {
+  class double_link {
+  public:
+
+    // =================================================================================================================
+    // Typedefs
+    // =================================================================================================================
+    
+    using pointer   = double_link *;
+    using reference = double_link &;
+
+    struct const_iterator;
+    
+    // =================================================================================================================
+    // Member fields
+    // =================================================================================================================
+
+    pointer prev;
+    pointer next;
+
+    // =================================================================================================================
+    // Constructors
+    // =================================================================================================================
+
+    constexpr double_link(): prev(nullptr), next(nullptr) {}
+    
+    // =================================================================================================================
+    // Printing-related member functions
+    // =================================================================================================================
+    
+    inline void describe_instance() {
+      print_line();
+      print_bits<true, false>("link is at", reinterpret_cast<uintptr_t>(this));
+      print_bits<true, false>("next", reinterpret_cast<uintptr_t>(next));
+      print_bits<true, false>("prev", reinterpret_cast<uintptr_t>(prev));
+    }
+
+    // =================================================================================================================
+    // Other member functions
+    // =================================================================================================================
+    
+    // inline void connect(pointer next_, pointer prev_) {
+    //   if (nullptr != next) next_->prev = this;
+    //   next        = next_;
+    //   prev        = prev_;
+    //   if (nullptr != prev) prev->next  = this;      
+    // }
+    // inline void connect(pointer head) {
+    //   connect(head, head->next);
+    // }
+                        
+    inline void insert_before(pointer next_) {
+      assert(nullptr != next_);
+
+      prev = next_->prev;
+      next = next_;
+      next->prev = this;
+      if (nullptr != prev) prev->next = this;
+    }
+    inline void insert_before(reference next_) {
+      insert_before(&next_);
+    }
+
+    inline void insert_after(pointer prev_) {
+      assert(nullptr != prev_);
+
+      prev = prev_;
+      next = prev_->next;
+      if (nullptr != next) next->prev = this;
+      prev->next = this;
+    }
+    inline void insert_after(reference prev_) {
+      insert_after(&prev_);
+    }
+
+    inline void remove() {
+      if (nullptr != next) next->prev = prev;
+      if (nullptr != prev) prev->next = next;
+
+      next = prev = nullptr;
+    }
+
+    // =================================================================================================================
+    // Iterator-related member functions
+    // =================================================================================================================
+    
+    // auto begin() const {
+    //   return const_iterator { this };
+    // }
+
+    // auto end() const {
+    //   return const_iterator { nullptr };
+    // }
+    
+    // =================================================================================================================
+  };
+}
+
+#endif
