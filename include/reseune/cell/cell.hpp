607b18a86cfda7284097fd57e5072d03e44b2473
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 02:12:58 -0400

    ...

--- include/reseune/cell/cell.hpp
+++ include/reseune/cell/cell.hpp
@@ -224,10 +224,14 @@ namespace reseune {
       // print_bits("cell.data:                 ", data);
       // print_bits("cell.tag():                ", tag());
       printf("cell.tag() as c_str:        %s\n", tag_type_as_c_str(tag()));
+
       if (is_type(tag_type::link))
         print_bits("cell.link():               ", reinterpret_cast<uintptr_t>(link()));
       else
         print_bits("cell.value()               ", value());
+
+      print_bits("something:                 ", 8u);
+      
       putchar('\n');
     }
 
@@ -282,7 +286,7 @@ namespace reseune {
       {
         uchar_type ix { 0 };
             
-        for (T mask { 1ul << ((sizeof(T) << 3) - 1) };
+        for (T mask { static_cast<T>(1) << ((sizeof(T) << 3) - 1) };
              mask;
              mask >>= 1) {
           if (show_bits)
@@ -303,8 +307,8 @@ namespace reseune {
         printf(show_bits
                ? " = 0x%016lx = % 20lu\n"
                : "   0x%016lx = % 20lu\n",
-               tmp,
-               tmp);
+               (uintptr_t)tmp,
+               (uintptr_t)tmp);
       else [[unlikely]]
         putchar('\n');
     }
