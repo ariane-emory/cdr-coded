1d97b963a7aba907b204e935a3d510c7b33e0abb
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 00:46:19 -0400

    ...

--- include/reseune/mempool/mempool.hpp
+++ include/reseune/mempool/mempool.hpp
@@ -1,14 +1,21 @@
 #ifndef RESEUNE_MEMPOOL
 #define RESEUNE_MEMPOOL
 
+#define ALLOC_HEADER_SZ offsetof(alloc_node_t, block)
+
+#ifndef align_up
+#define align_up(num, align) (((num) + ((align)-1)) & ~((align)-1))
+#endif
+
 namespace reseune {
   template <typename T, size_t S>
   class mempool : public pool<T, S> {
-    using value_type = T;
-    
+    using base_type  = pool<T, S>;
+    using value_type = base_type::value_type;
+
     struct ll_t {
-      struct ll_head *next;
-      struct ll_head *prev;
+      struct ll_t *next;
+      struct ll_t *prev;
     };
 
     struct alloc_node_t {
@@ -16,7 +23,24 @@ namespace reseune {
       size_t size;
       T *    block;
     };
+
+    void malloc_addblock(void *addr, size_t size)
+      {
+        alloc_node_t *blk;
+
+        // align the start addr of our block to the next pointer aligned addr
+        blk = (void *) align_up((uintptr_t)addr, sizeof(void*));
+
+        // calculate actual size - mgmt overhead
+        blk->size = (uintptr_t) addr + size - (uintptr_t) blk 
+          - ALLOC_HEADER_SZ;
+
+        //and now our giant block of memory is added to the list!
+        list_add(&blk->node, &base_type::data);
+      }
   };
 }
+
+#undef ALLOC_HEADER_SZ
 
 #endif
