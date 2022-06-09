f2543f88448d3ed70f62c83ef7451ccc781ce885
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 19:52:51 -0400

    ...

--- include/reseune/reseune.hpp
+++ include/reseune/reseune.hpp
@@ -82,13 +82,17 @@ namespace reseune {
 }
 
 #include "reseune/const_iterator_base/const_iterator_base.hpp"
-#include "reseune/cell/cell.hpp"
 #include "reseune/array_forward_const_iterator/array_forward_const_iterator.hpp"
+#include "reseune/linked_list_forward_const_iterator/linked_list_forward_const_iterator.hpp"
+
+#include "reseune/cell/cell.hpp"
+
 #include "reseune/pool/pool.hpp"
 #include "reseune/mempool/mempool.hpp"
-#include "reseune/linked_list_forward_const_iterator/linked_list_forward_const_iterator.hpp"
+
 #include "reseune/link/link.hpp"
 #include "reseune/double_link/double_link.hpp"
+
 #include "reseune/alloc_block/alloc_block.hpp"
 
 #endif
