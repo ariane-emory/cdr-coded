b4a51da5b9267c2b932c7e08ec7fac989d627393
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 19:51:13 -0400

    ...

---
+++ include/reseune/array_forward_const_iterator/array_forward_const_iterator.hpp
@@ -0,0 +1,33 @@
+#ifndef RESEUNE_ARRAY_FORWARD_CONST_ITERATOR
+#define RESEUNE_ARRAY_FORWARD_CONST_ITERATOR
+
+namespace reseune {
+  template <typename TT>
+  struct array_forward_const_iterator : public const_iterator_base<TT>
+  {
+    using value_type = TT;
+    using base_type  = const_iterator_base<value_type>;
+      
+    constexpr array_forward_const_iterator(value_type const * ptr): base_type(ptr) {}
+      
+    // prefix
+    inline array_forward_const_iterator & operator ++ () {
+      base_type::m_value++;
+
+      return *this;
+    }
+
+    // postfix: untested 
+    inline array_forward_const_iterator operator ++ (int) {
+      array_forward_const_iterator tmp = *this;
+
+      ++(*this);
+
+      return tmp; 
+    } 
+  };
+
+  // ===================================================================================================================
+}
+
+#endif
