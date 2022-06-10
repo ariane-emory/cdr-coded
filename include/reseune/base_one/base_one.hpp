11f2ae0d92d21e658ea7530cc1ce853f07dae870
Ariane Emory <ariane.emory@protonmail.com>
2022-06-09 22:26:42 -0400

    move global data to reseune::base_one

---
+++ include/reseune/base_one/base_one.hpp
@@ -0,0 +1,23 @@
+#ifndef RESEUNE_BASE_ONE_HPP
+#define RESEUNE_BASE_ONE_HPP
+
+namespace reseune {
+  namespace base_one {
+    static doubly_linked<alloc_block> free_list;
+
+    // static void malloc_add_block(void * addr, size_t size) {
+    //   alloc_block * blk;
+      
+    //   // align the start addr of our block to the next pointer aligned addr
+    //   blk = reinterpret_cast<alloc_block *>(align_up((uintptr_t)addr, sizeof(void*)));
+
+    //   // calculate actual size - mgmt overhead
+    //   blk->size = (uintptr_t) addr + size - (uintptr_t) blk 
+         //     - ALLOC_HEADER_SZ;
+
+         //   //and now our giant block of memory is added to the list!
+         //   blk->node.insert_before(&free_list);
+         // }
+  }
+}
+#endif
