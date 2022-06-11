a8c1692be3ed27da516f2673afa90fcd3432daa2
Ariane Emory <ariane.emory@protonmail.com>
2022-06-11 00:44:57 -0400

    ...

--- include/reseune/alloc_info/alloc_info.hpp
+++ include/reseune/alloc_info/alloc_info.hpp
@@ -9,9 +9,9 @@ namespace reseune {
 
     void describe_instance(char c = '=') const {
       print_line(c);
-      print_bits<true, false>("alloc_info is at", reinterpret_cast<uintptr_t>(this));
+      print_bits<true, false>("alloc_info is at", uintptr(this));
       print_bits<true, false>("size",       size);
-      print_bits<true, false>("block",       reinterpret_cast<uintptr_t>(block));
+      print_bits<true, false>("block",       uintptr(block));
     }
   };
 }
