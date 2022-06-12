5c5b864945e13a724d94cc2b97091cf9d5ebd78a
Ariane Emory <ariane.emory@protonmail.com>
2022-06-12 04:48:23 -0400

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -177,17 +177,17 @@ char buff1[buff_len] {0};
 char buff2[buff_len] {0};
 
 void test_base_one() {
-  using namespace reseune::base_one::allocator;
+  using namespace reseune::base_one;
 
   const bool verbose {true};
 
   // initialize(verbose);
 
-  add_memory(buff1, buff_len, verbose);
-  add_memory(buff2, buff_len, verbose);
+  allocator::add_memory(buff1, buff_len, verbose);
+  allocator::add_memory(buff2, buff_len, verbose);
   
   if (verbose)
-    describe_free_list();
+    allocator::describe_free_list();
 
   using T = int;
   
@@ -200,13 +200,13 @@ void test_base_one() {
       printf("Request #%zu, requesting %u bytes.\n", ++ix, sizeof(T) * 1024);
     }
     
-    buffer = alloc<T>(1024, false); // Ignoring verbose!
+    buffer = allocator::alloc<T>(1024, false); // Ignoring verbose!
     
     if (verbose) {
       reseune::print_bits<verbose, false>("Received", reseune::uintptr(buffer));
       LINE;
       putchar('\n'); putchar('\n');
-      describe_free_list();
+      allocator::describe_free_list();
     }
 
     // release(buffer, verbose);
