7f913f0c0271062ee50ae0ca9c98734eb0bfc320
Ariane Emory <ariane.emory@protonmail.com>
2022-06-14 04:48:26 -0400

    still a bit broken but getting closer

--- include/reseune/allocator/strategies.hpp
+++ include/reseune/allocator/strategies.hpp
@@ -17,31 +17,30 @@ namespace reseune {
   namespace allocator_strategies {
 
     // =========================================================================================================
-    template <typename alloc_node>
+    template <typename alloc_node, typename placement>
     struct ordinary {};
     
     // =========================================================================================================
-    template <>
-    struct ordinary<alloc_info> {
-      using allocator_type = allocator<alloc_info>;
-      using alloc_node = typename allocator_type::alloc_node;
+    template <typename placement>
+    struct ordinary<alloc_info, placement> {
+      using alloc_node = typename allocator<alloc_info_with_unfree_flag>::alloc_node;
 
       static inline void commit_block(alloc_node & block, VERBOSEARG) {
         block.remove();
       }
 
-      static inline void release_block(alloc_node & block, alloc_node & head, VERBOSEARG); //  {
-      //   allocator_type::placement::place_block(block, head, verbose);
-      // }
+      static inline void release_block(alloc_node & block, alloc_node & head, VERBOSEARG) {
+        placement::place_block(block, head, verbose);
+      }
 
       static inline bool block_is_free(alloc_node const & block, VERBOSEARG) {
         return true;
       }
-      };
+    };
 
     // =========================================================================================================
-    template <>
-    struct ordinary<alloc_info_with_unfree_flag> {
+    template <typename placement>
+    struct ordinary<alloc_info_with_unfree_flag, placement> {
       using alloc_node = typename allocator<alloc_info_with_unfree_flag>::alloc_node;
 
       static inline void commit_block(alloc_node & block, VERBOSEARG) {
@@ -59,7 +58,7 @@ namespace reseune {
 
     // =========================================================================================================
   };
-  // ===========================================================================================================
+// ===========================================================================================================
 }
 // =============================================================================================================
 #endif
