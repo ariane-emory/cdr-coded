ece74d45bafd8f455d2bcfdad187028f89124716
Ariane Emory <ariane.emory@protonmail.com>
2022-06-13 20:18:52 -0400

    add missing

---
+++ include/reseune/alloc_info_with_unfree_flag/alloc_info_with_unfree_flag.hpp
@@ -0,0 +1,46 @@
+#ifndef RESEUNE_ALLOC_INFO_H
+#define RESEUNE_ALLOC_INFO_H
+
+namespace reseune {
+
+  // ===================================================================================================================
+
+  struct alloc_info_with_unfree_flag {
+    size_t size;
+    char * block_start;
+    bool   unfree;
+
+
+    void describe_instance() const {
+      print_bits<true, false>("alloc_info is at", uintptr(this));
+      print_bits<true, false>("size", size);
+      printf(
+        "state               : %s\n",
+        unfree
+        ? "unfree"
+        : "free");
+    }
+  };
+
+  // ===================================================================================================================
+
+  template <>
+  inline bool allocator<alloc_info_with_unfree_flag>::is_free(
+    allocator<alloc_info_with_unfree_flag>::alloc_node & block) {    
+    return ! block.data.unfree;
+  }
+    
+  template <>
+  inline void allocator<alloc_info_with_unfree_flag>::unfree_block(
+    allocator<alloc_info_with_unfree_flag>::alloc_node & block) {
+    block.data.unfree = true;
+  }
+
+  template <>
+  inline void allocator<alloc_info_with_unfree_flag>::release_blockp(
+    allocator<alloc_info_with_unfree_flag>::alloc_node * pblock, bool verbose) {
+    pblock->data.unfree = false;
+  }
+}
+
+#endif
