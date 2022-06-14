2b7998813a56a9654944022b2f83bde32a52d9c4
Ariane Emory <ariane.emory@protonmail.com>
2022-06-14 00:21:47 -0400

    ...

--- include/reseune/allocator/allocator.hpp
+++ include/reseune/allocator/allocator.hpp
@@ -10,47 +10,9 @@
 #include "reseune/doubly_linked/doubly_linked.hpp"
 
 #include "macros.hpp"
+#include "strategies.hpp"
 
 namespace reseune {
-  // ===========================================================================================================
-  
-  template <typename T>
-  class allocator;
-
-  // ===========================================================================================================
-
-  struct strategies {
-    template <typename alloc_node_t>
-    struct no_track {
-      static inline void commit_block(alloc_node_t & block) {
-        block.remove();
-      }
-
-      static inline void release_block(alloc_node_t & block, VERBOSEARG) {
-        allocator<alloc_node_t>::place_block(block, verbose);
-      }
-
-      static inline bool block_is_free(alloc_node_t const & block) {
-        return true;
-      }
-    };
-
-    template <typename alloc_node_t>
-    struct track_by_marking {
-      static inline void commit_block(alloc_node_t & block) {
-        block.data.unfree = true;
-      }
-
-      static inline void release_block(alloc_node_t & block, VERBOSEARG) {
-        block.data.unfree = false;
-      }
-
-      static inline bool block_is_free(alloc_node_t const & block) {
-        return ! block.data.unfree;
-      }
-    };
-  };
-
   // ===========================================================================================================
   
   template <typename T>
