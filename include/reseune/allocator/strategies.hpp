0cb4e1eed5672c378a0ad1c40836960e1efc819a
Ariane Emory <ariane.emory@protonmail.com>
2022-06-14 03:19:13 -0400

    ...

--- include/reseune/allocator/strategies.hpp
+++ include/reseune/allocator/strategies.hpp
@@ -15,8 +15,44 @@ namespace reseune {
 
     // =========================================================================================================
 
-    template <typename value_type>
+    template <typename alloc_node>
     struct doubly_linked_placement_stategy {
+      // =======================================================================================================
+    
+    public:
+    
+      static inline void place_block(alloc_node & new_block, alloc_node & head, VERBOSEARG) {
+        PRLINE;
+        PRINT("Placing block", &new_block);
+
+        alloc_node * plast_block {nullptr}; 
+ 
+        FOR_EACH_BLOCK(head) {
+#ifndef NDEBUG
+          if (plast_block == &block) 
+            DIE("last_blook == block, this is probably a logic error.\n");
+#endif
+
+          plast_block = &block; 
+          
+          PRINT("Compare with", uintptr(&block));
+              
+          if (plast_block <= &new_block) 
+            continue;
+                
+          PRINTF("Placed block is before this block.\n"); 
+
+          CONS(new_block, block); 
+
+          return;
+        } 
+
+        // There shouldn't be any way for plast_block to be null if we got this far.
+              
+        PRINTF("Placed block is at the end.\n");
+              
+        RCONS(new_block, plast_block); 
+      }
     };
     
     // =========================================================================================================
