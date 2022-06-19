8160d3c5306daac5b4790f80fbbba21a91572d71
Ariane Emory <ariane.emory@protonmail.com>
2022-06-19 07:18:44 -0400

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,21 @@ using t = tokenizer<l>;
 
 // ===============================================================================================================
 int main() {
-  // const char * const input    {"(((abcdefg wo_go_99 0 three four\n (five six) seven\n eight)" };
-  const char * const input    {"0 123 00123 -12 -012 -33"};
+  const char * const input    {"(((abcdefg wo_go_99 0 0 123 00123 -12 -012 -33 three four\n (five six) seven\n eight)" };
+  // const char * const input    {"0 123 00123 -12 -012 -33"};
   t                  tokenizer{input};
   t::span            result   {};
   
   do {
-    result = tokenizer.strip<tok label<integer, tok integer>>();
-    // result = tokenizer.strip<
-    //   tok either<
-    //     tok label<symbol,  tok c_style_identifier>,
-    //     tok either <
-    //       tok label<integer, tok positive_integer>,
-    //       tok either<
-    //         tok label<l_paren, tok character<'('>>,
-    //         tok label<r_paren, tok character<')'>>>>>>();
+    // result = tokenizer.strip<tok label<integer, tok integer>>();
+    result = tokenizer.strip<
+      tok either<
+        tok label<symbol,  tok c_style_identifier>,
+        tok either <
+          tok label<integer, tok integer>,
+          tok either<
+            tok label<l_paren, tok character<'('>>,
+            tok label<r_paren, tok character<')'>>>>>>();
     
     if (result)
       printf("Token is (%u, '%s').\n", result.label, result.c_str());
