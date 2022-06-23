4d8eab4b3deb65b6b11d84983da460a0d64a65a5
Ariane Emory <ariane.emory@protonmail.com>
2022-06-23 05:29:25 -0400

    ...

--- include/reseune/tokenizer/tokenizer.hpp
+++ include/reseune/tokenizer/tokenizer.hpp
@@ -353,36 +353,23 @@ namespace reseune {
     // ===================================================================================================================
     MATCH_F(lispesque_keyword) {
       // Match a set of strings that look like reasonable Lisp keyword symbol names.
-      constexpr auto HEAD_MF {my all<my character<':'>, my alpha, my star<my alnums>>};
+      constexpr auto HEAD_MF      {my all<my character<':'>, my alpha, my star<my alnums>>};
       constexpr auto SEPARATOR_MF {my plus<my character<'-'>>};
-      constexpr auto TAIL_MF {my plus<my alnums>};
-      constexpr auto MF {my intercalate<HEAD_MF, SEPARATOR_MF, TAIL_MF>};
+      constexpr auto TAIL_MF      {my plus<my alnums>};
+      constexpr auto MF           {my intercalate<HEAD_MF, SEPARATOR_MF, TAIL_MF>};
 
       START;
       MATCH;
       RETURN_MATCH;
-
-      // return with_lispesque_token_terminator<
-      //     my all<
-      //     HEAD_MF,
-      //     my star<
-      //       my all<
-      //         SEPARATOR_MF,
-      //         TAIL_MF>>,
-      //     my optional<
-      //       my any<
-      //         my character<'!'>,
-      //         my character<'?'>>>>>();
     }
 
     // ===================================================================================================================
     MATCH_F(lispesque_identifier) {
       // Match a set of strings that look like reasonable Lisp symbol names.
-      constexpr auto HEAD_MF {my all<my alpha, my star<my alnums>>};
+      constexpr auto HEAD_MF      {my all<my alpha, my star<my alnums>>};
       constexpr auto SEPARATOR_MF {my any<my plus<my character<'-'>>, my plus<my character<':'>>, my plus<my character<'/'>>>};
-      constexpr auto TAIL_MF {my plus<my alnums>};
-      constexpr auto MF {my intercalate<HEAD_MF, SEPARATOR_MF, TAIL_MF>};
-
+      constexpr auto TAIL_MF      {my plus<my alnums>};
+      constexpr auto MF           {my intercalate<HEAD_MF, SEPARATOR_MF, TAIL_MF>};
       
       return with_lispesque_token_terminator<
         my any<
