889005c4f50dd6bd789fc1c7297bb844763bbaaf
Ariane Emory <ariane.emory@protonmail.com>
2022-06-24 19:58:49 -0400

    ...

--- include/reseune/delete_traits/delete_traits.hpp
+++ include/reseune/delete_traits/delete_traits.hpp
@@ -13,19 +13,19 @@ namespace reseune {
     // =================================================================================================================
     template<typename T1>
     struct owner {
-      static void destroy(T1&) {}
+      static void destroy(T1&) { printf("Destroy 1.\n"); }
     };
     
     // =================================================================================================================
     template<typename T1>
     struct owner<T1*> {
-      static void destroy(T1* p) { delete p; }
+      static void destroy(T1* p) {  printf("Destroy 2.\n"); delete p; }
     };
 
     // =================================================================================================================
     template<typename T1>
     struct non_owner {
-      static void destroy(T1&) {}
+      static void destroy(T1&) {  printf("Destroy 3.\n"); }
     };
 
     // =================================================================================================================
