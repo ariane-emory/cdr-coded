4ad2c9f498e1a0bf2dd5e9ff34ebaa7f89c2eb2a
Ariane Emory <ariane.emory@protonmail.com>
2022-06-23 20:46:54 -0400

    ...

--- include/reseune/tokenizer/lispesque_tokenizer.hpp
+++ include/reseune/tokenizer/lispesque_tokenizer.hpp
@@ -5,6 +5,7 @@
 
 #define BASES_MATCH_F(name) static constexpr span_t(tokenizer<tt>::*name)()
 #define rule static constexpr match_f
+#define __ my
 
 #include "macros.hpp" // include last!
 
@@ -40,23 +41,23 @@ namespace reseune {
     // Declare this rule a little early since we're going to use it while making primitives:
     // =================================================================================================================
     rule Lispesque_token_terminator =
-      my any<my whitespace,
-             my character<')'>>;
+      __ any<__ whitespace,
+             __ character<')'>>;
     
     // =================================================================================================================
     // Pointers to match_f templates in base.
     // =================================================================================================================
-    T_MATCH_F BASES_MATCH_F(terminated)   = my all<MF, my followed_by<    Lispesque_token_terminator>>;
-    T_MATCH_F BASES_MATCH_F(unterminated) = my all<MF, my not_followed_by<Lispesque_token_terminator>>;
+    T_MATCH_F BASES_MATCH_F(terminated)   = __ all<MF, __ followed_by<    Lispesque_token_terminator>>;
+    T_MATCH_F BASES_MATCH_F(unterminated) = __ all<MF, __ not_followed_by<Lispesque_token_terminator>>;
 
     template <char... Cs>
-    BASES_MATCH_F(terminated_word)        = terminated<my word<Cs...>>;
+    BASES_MATCH_F(terminated_word)        = terminated<__ word<Cs...>>;
     
     // =================================================================================================================
     // Manufacture match_fs for common operator-like symbols as terminated_words as well as some primitive symbols.
     // =================================================================================================================
 #define X(...) terminated_word<__VA_ARGS__>
-#define Y(name) rule name = my any<name ## s>
+#define Y(name) rule name = __ any<name ## s>
     Y(boolean_op);
     Y(increment_decrement_op);
     Y(other_comparison_op);
@@ -70,74 +71,74 @@ namespace reseune {
     // =================================================================================================================
     // Grammar rules.
     // =================================================================================================================
-    rule Lispesque_Operator = my any<
+    rule Lispesque_Operator = __ any<
       boolean_op,
       other_math_op,
       other_comparison_op,
       increment_decrement_op>;
 
-    rule Keyword_Separator = my characters<'-'>;
+    rule Keyword_Separator = __ characters<'-'>;
 
     rule Lispesque_Primitive =
-      my label<primitive,
-               my any<primitive_math_op,
+      __ label<primitive,
+               __ any<primitive_math_op,
                       primitive_comparison_op,
                       primitive_symbol>>;
 
     rule Symbol_Head =
-      my all<my alpha,
-             my star_alnums>;
+      __ all<__ alpha,
+             __ star_alnums>;
       
     rule Keyword_Head =
-      my all<my character<':'>,
+      __ all<__ character<':'>,
              Symbol_Head>;
 
     rule Keyword_Body =
-      my intercalate<Keyword_Head,
+      __ intercalate<Keyword_Head,
                      Keyword_Separator,
-                     my alnums>;
+                     __ alnums>;
       
     rule Lispesque_Keyword =
-      my label<keyword,
+      __ label<keyword,
                terminated<Keyword_Body>>;
 
     rule Symbol_Separator =
-      my any<my characters<'-'>,
-             my characters<':'>,
-             my characters<'/'>>;
+      __ any<__ characters<'-'>,
+             __ characters<':'>,
+             __ characters<'/'>>;
       
     rule Symbol_Body =
-      my intercalate<Symbol_Head,
+      __ intercalate<Symbol_Head,
                      Symbol_Separator,
-                     my alnums>;
+                     __ alnums>;
       
-    rule Symbol_Trailer = my optional<my character<'!','?'>>;
+    rule Symbol_Trailer = __ optional<__ character<'!','?'>>;
       
     rule Lispesque_Symbol =
-      my label<symbol,
-               terminated<my any<Lispesque_Operator,
-                                 my all<Symbol_Body,
+      __ label<symbol,
+               terminated<__ any<Lispesque_Operator,
+                                 __ all<Symbol_Body,
                                         Symbol_Trailer>>>>;
 
     rule LParen =
-      my label<l_paren,
-               my character<'('>>;
+      __ label<l_paren,
+               __ character<'('>>;
 
     rule RParen =
-      my label<r_paren,
-               terminated<my character<')'>>>;
+      __ label<r_paren,
+               terminated<__ character<')'>>>;
 
     rule Quote =
-      my label<quote,
-               unterminated<my character<'\''>>>;
+      __ label<quote,
+               unterminated<__ character<'\''>>>;
 
     rule Integer =
-      my label<tt::integer,
-               terminated<my integer>>;
+      __ label<tt::integer,
+               terminated<__ integer>>;
       
     rule Token =
-      my strip<
-      my any<LParen,
+      __ strip<
+      __ any<LParen,
              RParen,
              Quote,
              Integer,
@@ -160,6 +161,7 @@ namespace reseune {
 #undef BASES_MATCH_F
 #undef BASES_T_MATCH_F
 #undef rule
+#undef __
 
 #include "undef_macros.hpp"
 // =====================================================================================================================
