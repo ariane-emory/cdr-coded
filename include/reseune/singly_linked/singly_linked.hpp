6fee47e196466f5d9168fb7dc10218f3c7c899d6
Ariane Emory <ariane.emory@protonmail.com>
2022-06-24 20:55:59 -0400

    ...

--- include/reseune/singly_linked/singly_linked.hpp
+++ include/reseune/singly_linked/singly_linked.hpp
@@ -31,7 +31,7 @@ namespace reseune {
     // =================================================================================================================
     // Printing-related member functions
     // =================================================================================================================
-    inline void describe_instance() const {
+    constexpr void describe_instance() const {
       print_line();
       print_bits<true, false>("singly_linked is at", uintptr(this));
       print_bits<true, false>("next",       uintptr(next));
@@ -40,22 +40,22 @@ namespace reseune {
     // =================================================================================================================
     // Other member functions
     // =================================================================================================================
-    inline void insert_before(pointer next_) {
+    constexpr void insert_before(pointer next_) {
       next = next_;
     }
     
-    inline void insert_before(reference next_) {
+    constexpr void insert_before(reference next_) {
       insert_before(&next_);
     }
 
     // =================================================================================================================
     // Iterator-related member functions
     // =================================================================================================================
-    inline auto begin() const {
+    constexpr auto begin() const {
       return const_iterator::begin(this);
     }
 
-    inline auto end() const {
+    constexpr auto end() const {
       return const_iterator::end();
     }
     
