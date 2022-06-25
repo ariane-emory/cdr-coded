6d8c58c7b0a94573f50a49b63b5fc29e7e51244c
Ariane Emory <ariane.emory@protonmail.com>
2022-06-24 20:03:08 -0400

    may have fixed leak

--- include/reseune/delete_traits/delete_traits.hpp
+++ include/reseune/delete_traits/delete_traits.hpp
@@ -2,6 +2,7 @@
 #define RESEUNE_DELETE_TRAITS_H
 
 #include <inttypes.h>
+#include <type_traits>
 
 // =====================================================================================================================
 namespace reseune {
@@ -13,14 +14,22 @@ namespace reseune {
     // =================================================================================================================
     template<typename T1>
     struct owner {
-      static void destroy(T1&) { printf("Destroy 1.\n"); }
+      static void destroy(T1 & x) {
+        if constexpr (std::is_pointer<T1>::value) {
+          printf("Destroy 1.\n");
+          delete x;
+        }
+        else {
+          printf("Destroy 2.\n");
+        }
+      }
     };
     
     // =================================================================================================================
-    template<typename T1>
-    struct owner<T1*> {
-      static void destroy(T1* p) {  printf("Destroy 2.\n"); delete p; }
-    };
+    // template<typename T1>
+    // struct owner<T1*> {
+    //   static void destroy(T1* p) {  printf("Destroy 2.\n"); delete p; }
+    // };
 
     // =================================================================================================================
     template<typename T1>
